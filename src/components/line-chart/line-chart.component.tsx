831cd0a766bc469f1b397520c8953f173f5d5956
ED <granrr.z@gmail.com>
2023-11-15 19:27:38 +0900

    DataStore added

---
+++ src/components/line-chart/line-chart.component.tsx
@@ -0,0 +1,63 @@
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Tooltip,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+import ChartDataPick from '../chart-data-pick/chart-data-pick.component'
+import { dataType } from '../../store/store'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip)
+
+export default function LineChart(props: dataType) {
+	const { data, labels } = props
+
+	const chartData = {
+		labels: labels,
+		datasets: [
+			{
+				data: data,
+				borderColor: 'rgb(53, 162, 235)',
+				backgroundColor: 'rgb(53, 162, 235)',
+				borderWidth: 4,
+				tension: 0.5,
+			},
+		],
+	}
+
+	const options = {
+		responsive: true,
+		scales: {
+			y: {
+				grid: { display: false },
+				ticks: { display: false },
+			},
+		},
+	}
+
+	return (
+		<>
+			<ChartDataPick average={5} highest={10} lowest={-5} scale="%" />
+			<Line options={options} data={chartData} />
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					padding: '0 1%',
+				}}
+			>
+				{data
+					? data.map((item, index) => (
+							<p key={index} style={{ fontSize: '0.75rem' }}>
+								{item}%
+							</p>
+					  ))
+					: null}
+			</div>
+		</>
+	)
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,15 +2,24 @@ import { create } from 'zustand'
 
 import { ResolutionType } from '../hooks/useResponsive'
 
-export interface NavBarState {
-	isOpened: boolean
-	setIsOpened: () => void
-	resetIsOpened: () => void
-}
+// export interface NavBarState {
+// 	isOpened: boolean
+// 	setIsOpened: () => void
+// 	resetIsOpened: () => void
+// }
 
 export interface ResState {
 	resolution: ResolutionType
 	setResolution: (newRes: ResolutionType) => void
+}
+
+export interface dataType {
+	data: number[]
+	labels: string[]
+}
+
+interface DataState {
+	pnlData: dataType
 }
 
 // export const useNavBarStore = create<NavBarState>((set) => ({
@@ -23,4 +32,12 @@ export const useResStore = create<ResState>((set) => ({
 	resolution: { isDesktop: true, isTablet: false, isMobile: false },
 	setResolution: (newRes: ResolutionType) =>
 		set(() => ({ resolution: newRes })),
+}))
+
+// ↓ Data from the server
+export const useDataStore = create<DataState>((set) => ({
+	pnlData: {
+		data: [2, 3, 4, 5, 4, 7, 8],
+		labels: ['11.09', '11.10', '11.11', '11.12', '11.13', '11.14', '11.15'],
+	} as dataType,
 }))
